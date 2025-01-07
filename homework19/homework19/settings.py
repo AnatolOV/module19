25c9115305efc3f54568a208b9fe932c28f4d429
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2025-01-07 21:11:42 +0300

    task5

--- homework19/homework19/settings.py
+++ homework19/homework19/settings.py
@@ -73,11 +73,21 @@ WSGI_APPLICATION = 'homework19.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'homework19',
+        'USER': 'postgres',
+        'PASSWORD': '111111',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
